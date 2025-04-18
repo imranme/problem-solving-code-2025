9995ed6d3853a04c52e8f43fd039f720e7410fc5
imranme <tusharimran914076@gmail.com>
2025-04-18 23:50:06 +0600

    solve today sWap Case

---
+++ HackerRank'/upar.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/swap-case/problem
+
+def swap_case(s):
+    return ''.join([i.upper() if i.islower() else i.lower() for i in s])
+    
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
