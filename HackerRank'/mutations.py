f3fbc5721ba669f0a0e24da77c43ac2f1afe13ad
imranme <tusharimran914076@gmail.com>
2025-08-29 22:59:37 +0600

    hekarrank mutaios problem

--- HackerRank'/mutations.py
+++ HackerRank'/mutations.py
@@ -1,4 +1,10 @@
 # link: https://www.hackerrank.com/challenges/python-mutations/problem
-
 def mutate_string(string, position, character):
-    return f'{string[:position]}{character}{string[position+1:]}'
+    # একে slice বা list যেকোনো পদ্ধতি দিয়ে করো
+    return string[:position] + character + string[position+1:]
+
+if __name__ == '__main__':
+    s = input()              # প্রথম লাইন: স্ট্রিং
+    i, c = input().split()   # দ্বিতীয় লাইন: index এবং character
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
