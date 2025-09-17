28ba6813a36cd8b2b36965cf95d7643ff338464b
imranme <tusharimran914076@gmail.com>
2025-09-17 23:47:35 +0600

    today solved python easy problem

---
+++ leetcode/28_find_the_index.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/sub
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+        
