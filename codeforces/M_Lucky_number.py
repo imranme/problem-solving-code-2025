2e980a3a406d4bc89e82d53990b88f19f45a9b9c
imranme <tusharimran914076@gmail.com>
2025-09-19 09:32:31 +0600

    today solved python easy problem

---
+++ codeforces/M_Lucky_number.py
@@ -0,0 +1,12 @@
+a, b = map(int, input("").split())
+
+lucky =[]
+
+for i in range(a,b+1):
+    if all(ch in "47" for ch in str(i)):
+        lucky.append(str(i))
+
+if lucky:
+    print(" ".join(lucky))
+else:
+    print("-1")
